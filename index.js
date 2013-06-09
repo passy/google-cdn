65fe2d93b92b4a0b5a0d0130aa78bd693df338f4
Pascal Hartig <phartig@rdrei.net>
2013-06-10 00:05:14 +0200

    Added tests

--- index.js
+++ index.js
@@ -3,21 +3,35 @@
 var semver = require('semver');
 var debug = require('debug')('google-cdn');
 
-var replacements = require('lib/data').replacements;
-var versions = require('lib/data').versions;
-var bowerUtil = require('util/bower');
-var hoist = require('util/hoist');
+var replacements = require('./lib/data').replacements;
+var versions = require('./lib/data').versions;
+var bowerUtil = require('./util/bower');
+var hoist = require('./util/hoist');
 
 var CDN_AJAX_PATH = '//ajax.googleapis.com/ajax/libs/';
 
 
-module.exports.cdnify = function (content, bowerJson, options) {
+function getVersionStr(bowerJson, name) {
+  var versionStr;
+  if (bowerJson.dependencies) {
+    versionStr = bowerJson.dependencies[name];
+  }
+
+  if (!versionStr && bowerJson.devDependencies && bowerJson.devDependencies[name]) {
+    versionStr = bowerJson.devDependencies[name];
+  }
+
+  return versionStr;
+}
+
+
+module.exports = function cdnify(content, bowerJson, options) {
   options = options || {};
   options.componentsDirectory = options.componentsDirectory || 'bower_components';
 
   Object.keys(replacements).forEach(function (item) {
     var replacement = replacements[item];
-    var versionStr = bowerJson.dependencies[item] || bowerJson.devDependencies[item];
+    var versionStr = getVersionStr(bowerJson, item);
 
     if (!versionStr) {
       return;
@@ -25,15 +39,15 @@ module.exports.cdnify = function (content, bowerJson, options) {
 
     var version = semver.maxSatisfying(versions[item], versionStr);
     if (version) {
-      debug('Choosing version %s for dependency %s.', version, item);
+      debug('Choosing version %s for dependency %s', version, item);
 
       var from = bowerUtil.joinComponent(options.componentsDirectory, replacement.from);
       var to = replacement.to(version);
       content = content.replace(from, to);
 
-      debug('Replaced %s with %s.', from, to);
+      debug('Replaced %s with %s', from, to);
     } else {
-      debug('Could not find satisfying version for %s %s.', item, versionStr);
+      debug('Could not find satisfying version for %s %s', item, versionStr);
     }
   });
 
--- test.js
+++ test.js
@@ -1,11 +1,47 @@
-/*global describe, it */
+/*global describe, it, beforeEach */
 
 'use strict';
-var assert = require('assert');
-var cdn = require('./index');
+var path = require('path');
+var assert = require('chai').assert;
+var proxyquire = require('proxyquire');
+var cdnify = require('./index');
 
 describe('google-cdn', function () {
   it('should load', function () {
-    assert(cdn !== undefined);
+    assert(cdnify !== undefined);
+  });
+
+  it('should replace jquery', function () {
+    var source = '<script src="bower_components/jquery/jquery.js"></script>';
+    var bowerConfig = {
+      dependencies: { jquery: '~2.0.0' }
+    };
+
+    var result = cdnify(source, bowerConfig);
+    assert.equal(result, '<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>');
+  });
+});
+
+describe('util/bower', function () {
+  beforeEach(function () {
+    this.pathMock = {
+      join: path.join,
+      sep: '/'  // Always test the unix way by default.
+    };
+
+    this.util = proxyquire('./util/bower', {
+      path: this.pathMock
+    });
+  });
+
+  it('joinComponent', function () {
+    var result = this.util.joinComponent('bower_components', 'jquery/jquery-2.0.js');
+    assert.equal(result, 'bower_components/jquery/jquery-2.0.js');
+  });
+
+  it('should join on w32', function () {
+    this.pathMock.sep = '\\';
+    var result = this.util.joinComponent('bower_components', 'jquery\\jquery-2.0.js');
+    assert.equal(result, 'bower_components/jquery/jquery-2.0.js');
   });
 });
