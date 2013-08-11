104e1aa28bc800ff3980f01df557044e29dee332
Pascal Hartig <phartig@rdrei.net>
2013-08-11 12:20:39 +0200

    feat(util): add bower main resolver
    util.bower.resolveMainPath can now resolve the main JS file of a bower
    component.

--- test.js
+++ test.js
@@ -2,9 +2,11 @@
 
 'use strict';
 var path = require('path');
+var fs = require('fs');
 var assert = require('chai').assert;
 var proxyquire = require('proxyquire');
 var googlecdn = require('./googlecdn');
+var EventEmitter = require('events').EventEmitter;
 
 describe('google-cdn', function () {
   it('should load', function () {
@@ -49,8 +51,26 @@ describe('util/bower', function () {
       sep: '/'  // Always test the unix way by default.
     };
 
+    this.processData = '';
+    this.spawnMock = function spawnMock() {
+      var ps = new EventEmitter();
+
+      ps.stdout = new EventEmitter();
+
+      process.nextTick(function () {
+        ps.stdout.emit('data', this.processData);
+        ps.emit('close', 0);
+      }.bind(this));
+
+      return ps;
+    }.bind(this);
+
     this.util = proxyquire('./util/bower', {
-      path: this.pathMock
+      path: this.pathMock,
+      /*jshint camelcase:false */
+      child_process: {
+        spawn: this.spawnMock
+      }
     });
   });
 
@@ -63,5 +83,53 @@ describe('util/bower', function () {
     this.pathMock.sep = '\\';
     var result = this.util.joinComponent('bower_components', 'jquery\\jquery-2.0.js');
     assert.equal(result, 'bower_components/jquery/jquery-2.0.js');
+  });
+
+  it('should resolve jquery to a main path', function (cb) {
+    this.processData = fs.readFileSync('./fixture/jquery-2.0.0.json');
+    this.util.resolveMainPath('jquery', '2.0.0', function (err, path) {
+      if (err) {
+        cb(err);
+      }
+
+      assert.equal(path, 'jquery/jquery.js');
+      cb();
+    });
+  });
+
+  it('should resolve jquery-ui to a main path', function (cb) {
+    this.processData = fs.readFileSync('./fixture/jquery-ui-1.10.3.json');
+    this.util.resolveMainPath('jquery-ui', '1.10.3', function (err, path) {
+      if (err) {
+        cb(err);
+      }
+
+      assert.equal(path, 'jquery-ui/ui/jquery-ui.js');
+      cb();
+    });
+  });
+
+  it('should resolve just js for multiple main files', function (cb) {
+    this.processData = fs.readFileSync('./fixture/multiple-main.json');
+    this.util.resolveMainPath('multiple-main', '1.10.3', function (err, path) {
+      if (err) {
+        cb(err);
+      }
+
+      assert.equal(path, 'multiple-main/lib/multiple.js');
+      cb();
+    });
+  });
+
+  it('should fall fall back to name w/o main', function (cb) {
+    this.processData = fs.readFileSync('./fixture/no-main.json');
+    this.util.resolveMainPath('no-main', '1.0', function (err, path) {
+      if (err) {
+        cb(err);
+      }
+
+      assert.equal(path, 'no-main/no-main.js');
+      cb();
+    });
   });
 });
--- util/bower.js
+++ util/bower.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var path = require('path');
+var spawn = require('child_process').spawn;
+var debug = require('debug')('google-cdn');
 var bowerUtil = module.exports;
 
 
@@ -10,4 +12,46 @@ bowerUtil.joinComponent = function joinComponent(directory, component) {
   // Always join the path with a forward slash, because it's used to replace the
   // path in HTML.
   return path.join(dirBits.join('/'), component).replace(/\\/g, '/');
+};
+
+
+function findJSMainFile(componentData) {
+  var main = componentData.main;
+  if (Array.isArray(main)) {
+    var js = main.filter(function (name) {
+      return (/\.js$/i).test(name);
+    });
+
+    if (js.length === 1) {
+      return js[0];
+    }
+  }
+
+  debug('Cannot determine main property');
+  return componentData.name.replace(/js$/i, '') + '.js';
+}
+
+
+bowerUtil.resolveMainPath = function resolveMain(component, version, callback) {
+  var args = ['info', '--json', component + '#' + version];
+  var output = '';
+  debug('resolving main property for component %s#%s', component, version);
+  var ps = spawn('bower', args, {
+    stdio: ['ignore', 'pipe', 'ignore']
+  });
+
+  ps.stdout.on('data', function (data) {
+    output += data;
+  });
+
+  ps.on('close', function (code) {
+    debug('bower exited with status code %d', code);
+    if (code !== 0) {
+      return callback(new Error('bower exited non-zero with ' + code));
+    }
+
+    var data = JSON.parse(output);
+    var main = findJSMainFile(data);
+    callback(null, data.name + '/' + main);
+  });
 };
