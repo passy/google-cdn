1bb390ab71d1f17d608b81afca24f240c7b81061
Pascal Hartig <phartig@rdrei.net>
2013-08-11 14:17:08 +0200

    doc(example): add full usage example

---
+++ example/runcdn.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var fs = require('fs');
+var googlecdn = require('../googlecdn.js');
+
+var index = fs.readFileSync('index.html', { encoding: 'utf-8' });
+var bowerJson = require('./bower.json');
+
+googlecdn(index, bowerJson, function (err, content) {
+  if (err) {
+    throw err;
+  }
+
+  fs.writeFileSync('dist.html', content);
+  console.log('Wrote cdnified version to dist.html');
+});
