259392944415d0f505786d838112bae711570ef3
Steve Mao <maochenyan@gmail.com>
2015-08-05 10:58:16 +1000

    chore(conventionalChangelog): correct config and add conventional-github-releaser
    You need to set environment variable GH_TOKEN as your github token.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,12 +25,36 @@ module.exports = function (grunt) {
         tasks: ['jshint:lib']
       }
     },
-    changelog: {}
+    conventionalChangelog: {
+      release: {
+        options: {
+          changelogOpts: {
+            preset: 'angular'
+          }
+        },
+        src: 'CHANGELOG.md'
+      },
+    },
+    conventionalGithubReleaser: {
+      release: {
+        options: {
+          auth: {
+            type: 'oauth',
+            token: process.env.GH_TOKEN
+          },
+          changelogOpts: {
+            preset: 'angular',
+            releaseCount: 0
+          }
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-conventional-github-releaser');
 
   grunt.registerTask('default', ['jshint']);
 };
